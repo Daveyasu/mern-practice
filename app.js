363debe4cd4a00fa110947d9953a7052c56c87de
Dawit <eyasu.dawit16@gmail.com>
2021-02-10 22:36:34 -0800

    fix the bug

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log('Hello World');
 console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
+// alert('THis is GIT');
+console.log('Asmera the Capital City');
+console.log('Hello World and Africa');
 console.log('Hello World');
