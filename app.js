1569beee9f3986a3211b1aae4b2ed18aa774d72e
Dawit <eyasu.dawit16@gmail.com>
2021-02-12 07:35:15 -0800

    .

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-console.log('Hello World');
-console.log('Hello World');
-// alert('THis is GIT');
-console.log('Asmera the Capital City');
-console.log('Hello World and Africa');
-console.log('Hello World');
+
+// function sum(a,b){
+//     return a*b
+// }
+
+// let sum=(a,b) => a*b;
+// console.log(sum(2,4));
+
+// function isPositiveNumber(){
+//     return num >=0;
+// }
+
+// let isPositiveNumber= () => Math.random();
+// console.log(isPositiveNumber());
+
+document.addEventListener('click',function(e){
+    console.log(e.target);
+})
+document.addEventListener('click',(e) => console.log(e.target))
